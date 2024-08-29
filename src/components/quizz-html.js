71a3ebe9a7af0ba847cae15624fbb2099c03fbbf
Lenny Servino <lennyservino@gmail.com>
2024-08-28 18:05:46 -0600

    agregando quizz-html.js

---
+++ src/components/quizz-html.js
@@ -0,0 +1,8 @@
+//arreglo que contiene las preguntas del quiz html
+const preguntasHTML = [
+    {
+        pregunta: "¿Cual es la etiqueta correcta para crear un hipervinculo?",
+        respuestas: ["<link>", "<a>", "<href>", "<hyperlink>"],
+        correcta: "<a>"
+    }
+];
